a41b0b8c62854037adff56db1dfc4032cbb7fae5
Salva <salva@rincewind.home>
2019-04-05 18:19:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Salva’s dotfiles
+
+This repo contains my personal dotfiles and are heavily based on [mathias](https://github.com/mathiasbynens/dotfiles/)'s and [Addy](https://github.com/addyosmani/dotfiles).
+
+
+## Installation
+
+### Using Git and the bootstrap script
+
+You can clone the repository wherever you want. (I like to keep it in `~/Projects/dotfiles`, with `~/dotfiles` as a symlink.) The bootstrapper script will pull in the latest version and copy the files to your home folder.
+
+```bash
+git clone https://github.com/mathiasbynens/dotfiles.git
+cd dotfiles
+source bootstrap.sh
+```
+
+### Sensible macOS defaults
+
+When setting up a new Mac, you may want to set some sensible macOS defaults:
+
+```bash
+./.macos
+```
+
+### Install Homebrew formulae
+
+When setting up a new Mac, you may want to install some common [Homebrew](https://brew.sh/) formulae (after installing Homebrew, of course):
+
+```bash
+./brew.sh
+```
+
+Some of the functionality of these dotfiles depends on formulae installed by `brew.sh`. If you don’t plan to run `brew.sh`, you should look carefully through the script and manually install any particularly important ones. A good example is Bash/Git completion: the dotfiles use a special version from Homebrew.
---
+++ brew.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+
+# Install last version of Bash.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install `wget` with IRI support.
+# brew install wget --with-iri # don't work with this option
+brew install wget
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install develop dependences
+brew install python
+brew install node
+brew install grep
+brew install openssh
+
+# Install more recent versions of some macOS tools.
+brew install vim
+brew install grep
+brew install openssh
+
+# Install other useful binaries.
+brew install git
+brew install tree
+brew install z
+
+# Remove outdated versions from the cellar.
+brew cleanup
